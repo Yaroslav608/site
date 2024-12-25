d96882c267b6b62306099cff6b052b650ad022c1
Yaroslav608 <138230061+Yaroslav608@users.noreply.github.com>
2024-12-25 13:24:49 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,7 @@
+function showRegistrationForm() {
+    document.getElementById('registration-form').style.display = 'block';
+}
+
+function hideRegistrationForm() {
+    document.getElementById('registration-form').style.display = 'none';
+}
